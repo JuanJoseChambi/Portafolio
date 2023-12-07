58813f984ff58c7063b55426dbd397a9fe2eb4e7
Juan Jose <chambijuanjose05@gmail.com>
2023-12-06 23:46:25 -0300

    Feat/Grid Proyects

--- src/view/Main/Proyectos.tsx
+++ src/view/Main/Proyectos.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import MMarket from "../../assets/main/Proeyctos/MMarket.png"
+import RecipeFood from "../../assets/main/Proeyctos/RecipeFood.png"
+import JCDV from "../../assets/main/Proeyctos/JCDV.svg"
+import Tn from "../../assets/main/Proeyctos/Tn.png"
+
 
 function Proyectos() {
   return (
     <section className='bg-[#dcd9d9]'>
-      <section className='area min-h-xl'>Proyectos</section>
+      <section className='area min-h-xl flex justify-center items-center'>
+        <section className="w-[900px] h-[500px] my-20 grid grid-cols-3 grid-rows-3 gap-1 proyectos bg-redd-500">
+
+          <picture className="border-solid overflow-hidden border-black border  bg-blue-500 [grid-area:bigTopLeft]">
+            <img src={JCDV} alt="" />
+          </picture>
+          <article className="border-solid overflow-hidden border-black border  bg-blue-500 [grid-area:minTopCenter]">
+            <img src={RecipeFood} alt="" />
+          </article>
+          <article className="border-solid overflow-hidden border-black border  bg-blue-500 [grid-area:bigTopRight]">
+            <img src={MMarket} alt="" />
+          </article>
+          {/* <article className="border-solid border-black border  bg-blue-500 [grid-area:minBotLeft]">
+            Holaf
+          </article> */}
+          <article className="border-solid overflow-hidden border-black border  bg-blue-500 [grid-area:bigBotCenter]">
+            <img src={Tn} alt="" />
+          </article>
+          <article className="border-solid overflow-hidden border-black border  bg-blue-500 [grid-area:minBotRight]">
+            <img src={MMarket} alt="" />
+          </article>
+
+        </section>
+      </section>
     </section>
   )
 }
