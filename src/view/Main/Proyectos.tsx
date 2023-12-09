067e1161ec06bfcf0218445d5c133fb94ba9e2ad
Juan Jose <chambijuanjose05@gmail.com>
2023-12-09 20:04:30 -0300

    Feat/ Tecnologias Info Img

--- src/view/Main/Proyectos.tsx
+++ src/view/Main/Proyectos.tsx
@@ -1,7 +1,5 @@
-import MMarket from "../../assets/main/Proeyctos/MMarket.svg";
-import RecipeFood from "../../assets/main/Proeyctos/RecipeFood.png";
-import JCDV from "../../assets/main/Proeyctos/JCDV.svg";
-import Tn from "../../assets/main/Proeyctos/Tn.png";
+import { proyectsData } from "../../utilities/proyectsData/proyectsData";
+
 import { useState } from "react";
 import { tecnologias } from "../../assets/Tecnologias";
 
@@ -15,6 +13,7 @@ interface proyects {
   gridArea: string;
   descripcion: string;
   short?: boolean;
+  infoTec?: null | arrayTecnologias[]
 }
 
 interface arrayTecnologias {
@@ -25,117 +24,19 @@ interface arrayTecnologias {
 function Proyectos() {
   // console.log(tecnologias);
 
-  const [proyects, setProyects] = useState<proyects[]>([
-    {
-      tecnologias: [
-        "HTML",
-        "REACT",
-        "JAVASCRIPT",
-        "TAILWIND",
-        "GITHUB",
-      ],
-      titulo: "Jesus Camino de Vida",
-      fecha: "20 - jul - 20 Dic - 2023",
-      github: "github.com/JuanJose",
-      web: "github.com/JuanJose",
-      image: JCDV,
-      gridArea: "bigTopLeft",
-      descripcion: "sssss",
-    },
-    {
-      tecnologias: [
-        "HTML",
-        "CSS",
-        "REACT",
-        "JAVASCRIPT",
-        "REDUX",
-        "NODE",
-        "EXPRESS",
-        "POSTGRESQL",
-        "GITHUB",
-      ],
-      titulo: "Recipe Food",
-      fecha: "20 Jul - 30 Ago - 2023",
-      github: "github.com/JuanJose",
-      web: "github.com/JuanJose",
-      image: RecipeFood,
-      gridArea: "minTopCenter",
-      short: true,
-      descripcion: "sss",
-    },
-    {
-      tecnologias: [
-        "HTML",
-        "REACT",
-        "JAVASCRIPT",
-        "TAILWIND",
-        "GITHUB",
-      ],
-      titulo: "IJCDV",
-      fecha: "20 Jul - 30 Ago - 2023",
-      github: "github.com/JuanJose",
-      web: "github.com/JuanJose",
-      image: JCDV,
-      gridArea: "bigTopRight",
-      descripcion: "sss",
-    },
-    {
-      tecnologias: [
-        "HTML",
-        "CSS",
-        "REACT",
-        "JAVASCRIPT",
-        "TYPESCRIPT",
-        "SASS",
-        "REDUX",
-        "GITHUB",
-      ],
-      titulo: "TN",
-      fecha: "20 Jul - 30 Ago - 2023",
-      github: "github.com/JuanJose",
-      web: "github.com/JuanJose",
-      image: Tn,
-      gridArea: "bigBotCenter",
-      short: true,
-      descripcion: "uhihiu",
-    },
-    {
-      tecnologias: [
-        "HTML",
-        "CSS",
-        "REACT",
-        "JAVASCRIPT",
-        "REDUX",
-        "NODE",
-        "EXPRESS",
-        "POSTGRESQL",
-        "GIT",
-        "GITHUB",
-      ],
-      titulo: "MMarket",
-      fecha: "20 Jul - 30 Ago - 2023",
-      github: "github.com/JuanJose",
-      web: "github.com/JuanJose",
-      image: MMarket,
-      gridArea: "minBotRight",
-      descripcion:
-        "MMarket simplifica compras en tiendas locales a través de su plataforma en línea, beneficiando a tiendas y usuarios con opciones de compra en persona y entregas a domicilio. Su objetivo es ampliar su alcance.",
-    },
-  ]);
-
-  const [infoProyect, setInfoProyect] = useState<proyects | null>(null);
-  // console.log(infoProyect);
-
-  // Imagenes a la informacion de las cards-------------------------------------------------------------------------------------------------------------------------
+  const [proyects, setProyects] = useState<proyects[]>(proyectsData);
   
 
-  // const [proyect, setProyect] = useState()
+  const [infoProyect, setInfoProyect] = useState<proyects | null>(null);
+  console.log(infoProyect);
+
+  // Imagenes a la informacion de las cards-------------------------------------------------------------------------------------------------------------------------
 
   return (
     <section className="bg-[#d2d2d2]">
       <section className="area min-h-xl flex flex-col justify-center items-center pb-20 pt-20 space-y-24">
         <article className="relative flex justify-center items-end">
-          <h2 className="text-7xl sm:text-8xl text-start bg-greenn-500 text-[#adadad] select-none">
+          <h2 className="text-[60px] sm:text-8xl text-start bg-greenn-500 text-[#adadad] select-none">
             PROYECTOS
           </h2>
           <h2 className="absolute text-5xl text-neutral-800 font-bold tracking-widest drop-shadow-[10px_5px_5px_#262626]">
@@ -144,12 +45,12 @@ function Proyectos() {
         </article>
 
         {infoProyect ? (
-          <section className="w-[95%] min-h-[500px] relative flex flex-col lg:flex-row justify-between items-center space-y-10 lg:space-y-0 bg-redd-500">
+          <section className="w-[95%] min-h-[500px] relative flex flex-col lg:flex-row justify-between items-center space-y-10 lg:space-y-0 bg-re-500">
             <i
               onClick={() => setInfoProyect(null)}
               className="absolute top-0 right-0 lg:top-28 lg:left-0 text-3xl cursor-pointer text-violet  bx bx-x-circle"
             ></i>
-            <section className="w-almostFull lg:w-[55%] h-[200px] order-2 lg:order-1 flex flex-col  justify-start items-start space-y-5 bg-limed-500">
+            <section className="w-almostFull lg:w-[55%] h-[200px] order-2 lg:order-1 flex flex-col  justify-start items-start space-y-5 bg-limde-500">
               <article className="w-full bg-liem-500">
                 <article className="relative flex justify-between items-center bg-limee-500">
                   <h2 className="text-2xl font-semibold tracking-widest">
@@ -171,6 +72,16 @@ function Proyectos() {
                 <p className="text-xs">{infoProyect.fecha}</p>
               </article>
               <p className="font-light text-sm">{infoProyect.descripcion}</p>
+
+              <section className="w-full h-1/6 bg-limee-500 flex justify-around lg:justify-start items-center lg:space-x-3">
+              {infoProyect.infoTec?.map((tec) => (
+                  
+                    <picture className="bg-redd-500 h-[25px] hover:-translate-y-2 hover:scale-150 transition-[transform_drop-shadow] ease-in-out duration-500  flex flex-col justify-center items-center drop-shadow-[0px_10px_5px_black] hover:drop-shadow-[0px_15px_10px_black]">
+                      {tec.img}
+                    </picture>
+
+              ))}
+              </section>
               
             </section>
 
@@ -200,39 +111,36 @@ function Proyectos() {
           </section>
         ) : (
           <section className="w-almostFull lg:w-[850px] lg:h-[500px] grid proyectos lg:grid-cols-3 lg:grid-rows-3 gap-7 lg:gap-4 lg:proyectos-medium bg-redd-500">
-            {proyects?.map((proyect, i) => (
-              <picture key={i} onClick={() => setInfoProyect(proyect)} className={`max-lg:max-h-80 relative flex justify-center items-start border-none rounded-xl overflow-hidden shadow-[15px_15px_15px_0px_#696969] border cursor-pointer [grid-area:${proyect.gridArea}]`}>
-                <div className="absolute top-0 w-full h-full opacity-0 hover:opacity-100 hover:bg-[#000000c4] flex justify-center items-center z-10 transition-[background,opacity] duration-500">
-                  {
-                    (() => {
-                      let tecnologiasProyecto = tecnologias.filter(tecno => proyect.tecnologias.some(tecnoProyect => tecno.nombre === tecnoProyect  ))
+            {proyects?.map((proyect, i) => {
 
-                      // console.log(tecnologiasProyecto);
+                const tecnologiasProyecto = tecnologias.filter(tecno => proyect.tecnologias.some(tecnoProyect => tecno.nombre === tecnoProyect  ))
+                const infoTec = "infoTec"
 
-                      proyect.tecnologias = tecnologiasProyecto
-                      // console.log(proyect.tecnologias);
+                proyect[infoTec] = tecnologiasProyecto;
+
+                return (
+                (
+                  <picture key={i} onClick={() => setInfoProyect(proyect)} className={`max-lg:max-h-80 relative flex justify-center items-start border-none rounded-xl overflow-hidden shadow-[15px_15px_15px_0px_#696969] border cursor-pointer [grid-area:${proyect.gridArea}] `}>
+                    <div className="absolute top-0 w-full h-full opacity-0 hover:opacity-100 hover:bg-[#000000c4] flex justify-center items-center z-10 transition-[background,opacity] duration-500">
                       
-              
-                      return (
-                        <section className=" w-[80%] min-h-[20%] flex flex-col flex-wrap justify-center items-center bg-limee-500 space-y-3">
-                         <section className="flex justify-center items-center space-x-3 flex-wrap">
-                          {tecnologiasProyecto.map((tecno) => (
-                            <div key={tecno.nombre} className="hover:-translate-y-2 my-1 bg-bluee-500 drop-shadow-[0px_0px_15px_white] transition-transform duration-300 flex justify-center items-center bg-bluee-500 ">{tecno.img}</div>
-                          ))}
-                         </section>
-                          <button className="flex justify-center border-b border-violet-700 items-center space-x-2 py-0.5 ">
-                            <i className='text-white text-[10px] bx bx-fullscreen'></i>
-                            <p className="text-white text-[10px] font-extralight">MOSTRAR MAS</p>
-                          </button>
-                            
-                        </section>
-                      )
-                    })()
-                  }
-                </div>
-                <img src={proyect.image} alt="" className={`${proyect.short ? "lg:h-full" : null} hover:blur-sm w-full `}/>
-              </picture>
-            ))}
+                      <section className=" w-[80%] min-h-[20%] flex flex-col flex-wrap justify-center items-center bg-limee-500 space-y-3">
+                       <section className="flex justify-center items-center space-x-3 flex-wrap">
+                        {tecnologiasProyecto.map((tecno) => (
+                          <div key={tecno.nombre} className="hover:-translate-y-2 my-1 bg-bluee-500 drop-shadow-[0px_0px_15px_white] transition-transform duration-300 flex justify-center items-center bg-bluee-500 ">{tecno.img}</div>
+                        ))}
+                       </section>
+                        <button className="flex justify-center border-b border-violet-700 items-center space-x-2 py-0.5 ">
+                          <i className='text-white text-[10px] bx bx-fullscreen'></i>
+                          <p className="text-white text-[10px] font-extralight">MOSTRAR MAS</p>
+                        </button>
+                      </section>
+                    
+                    </div>
+                    <img src={proyect.image} alt="" className={`${proyect.short ? "lg:h-full" : null} hover:blur-sm w-full select-none`}/>
+                  </picture>
+                )
+              )
+            })}
           </section>
         )}
       </section>
