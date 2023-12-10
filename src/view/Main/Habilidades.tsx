19c058a073bc306e06dc8d01a256254bdab8a4a9
Juan Jose <chambijuanjose05@gmail.com>
2023-12-10 20:36:48 -0300

    Feat/Responsive Habilidades !

--- src/view/Main/Habilidades.tsx
+++ src/view/Main/Habilidades.tsx
@@ -7,18 +7,18 @@ function Habilidades() {
   const [borderBottom, setBorderBottom] = useState<null | string>(null)
 
   return (
-    <section className="area flex flex-col justify-start items-center min-h-xl bg-limee-500">
+    <section className="area flex flex-col justify-start items-center min-h-xl bg-lim-500">
         
        <article className="w-almostFull relative flex justify-start items-end mb-28 bg-re-500">
         <h2 className="text-8xl text-start bg-greenn-500 text-[#adadad8c] select-none">HABILIDADES</h2>
           <h2 className="absolute text-5xl text-neutral-800 font-bold tracking-widest drop-shadow-[10px_5px_5px_#262626]">HABILIDADES</h2>
        </article>
 
-       <section className="w-almostFull h-auto relative flex justify-between items-end bg-graye-500">
+       <section className="w-full h-auto relative flex flex-col lg:flex-row justify-between items-end bg-grayd-500 space-y-14 lg:space-y-0">
 
-        <article className="bg-blu-500 w-1/2 h-[313px] flex flex-col justify-between items-center space-y-5">
+        <article className="w-full lg:w-1/2 h-auto lg:h-[313px] flex flex-col justify-between items-center space-y-5">
           <h2 className="text-lg font-semibold tracking-spacing ">TECNICAS</h2>
-          <article className="w-[450px] flex flex-wrap justify-center items-center bg-greend-500 ">
+          <article className="w-full lg:w-[450px] flex flex-wrap justify-center items-center bg-greend-500 ">
             {tecnologias.map((tec) => (
                 <picture key={tec.nombre} className="h-[35px] relative flex flex-col justify-center items-center mx-3 my-5 bg-blu-500 drop-shadow-[3px_5px_3px_grey] hover:drop-shadow-[3px_15px_8px_grey] hover:-translate-y-3 transition-[transform_drop-shadow] duration-500" onMouseEnter={() => setBorderBottom(tec.nombre)} onMouseLeave={() => setBorderBottom(null)}>
                   {tec.img}
@@ -31,23 +31,23 @@ function Habilidades() {
           </article>
         </article>
 
-        <div className=" w-[1px] h-60 bg-violet select-none">ㅤ</div>
+        <div className="hidden lg:block w-[1px] h-60 bg-violet select-none">ㅤ</div>
         
-        <article className="bg-bluee-500 w-1/2 h-[313px] flex flex-col justify-between items-center">
-          <h2 className="text-lg font-semibold tracking-spacing">BLANDAS</h2>
-          <article className="w-[450px] h-[225px] flex flex-col justify-around items-center bg-lim-500">
+        <article className="w-full lg:w-1/2 h-auto lg:h-[313px] flex flex-col justify-between items-center">
+          <h2 className="text-lg font-semibold tracking-spacing max-sm:mb-7">BLANDAS</h2>
+          <article className="w-full lg:w-[450px] h-[225px] flex flex-col justify-around items-center bg-redd-500">
               
-              <article className="w-full flex justify-evenly text-lg font-light items-center bg-re-500">
-                <p className="w-[40%] text-center firstLetter">Autodidacta</p>
-                <p className="w-[40%] text-center firstLetter">Curiosidad</p>
+              <article className="w-full h-[75px] flex flex-wrap justify-evenly text-lg font-light items-center bg-redd-500">
+                <p className="w-[180px] text-center firstLetter bg-redd-500">Autodidacta</p>
+                <p className="w-[180px] text-center firstLetter">Curiosidad</p>
               </article>
-              <article className="w-full flex justify-evenly text-lg font-light items-center bg-re-500">
-                <p className="w-[40%] text-center firstLetter">Adaptabilidad</p>
-                <p className="w-[40%] text-center firstLetter">Aprendizaje Agil</p>
+              <article className="w-full h-[75px] flex flex-wrap justify-evenly text-lg font-light items-center bg-redd-500">
+                <p className="w-[180px] text-center firstLetter">Adaptabilidad</p>
+                <p className="w-[180px] text-center firstLetter">Aprendizaje Agil</p>
               </article>
-              <article className="w-full flex justify-evenly text-lg font-light items-center bg-re-500">
-                <p className="w-[40%] text-center firstLetter">Trabajo en Equipo</p>
-                <p className="w-[40%] text-center firstLetter">Perseverancia</p>
+              <article className="w-full h-[75px] flex flex-wrap justify-evenly text-lg font-light items-center bg-redd-500">
+                <p className="w-[180px] text-center firstLetter">Trabajo en Equipo</p>
+                <p className="w-[180px] text-center firstLetter">Perseverancia</p>
               </article>
 
           </article>
