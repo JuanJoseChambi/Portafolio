9b264e3fdee3a951ab851e9bffe5887bf25827ac
Juan Jose <chambijuanjose05@gmail.com>
2023-12-17 22:03:34 -0300

    Feat/Icon Web

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Logo from "../../assets/LogoJC.png"
 import { useScrollSection } from "../../hooks/useScrollSections";
-import ButtonRedirectHome from "../ButtonRedirectHome/ButtonRedirectHome";
 
 
 interface RefPortafolio {
