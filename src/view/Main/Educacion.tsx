07cff0bb9c13d9df8e2103ae7abecb6771e363f8
Juan Jose <chambijuanjose05@gmail.com>
2023-12-18 15:04:47 -0300

    Feat/Padding Education Responsive Bug

--- src/view/Main/Educacion.tsx
+++ src/view/Main/Educacion.tsx
@@ -11,7 +11,7 @@ function Educacion({refEducacion}:RefSobreMi) {
   useFadeScrollY(refScrollEducacion, "opacity-100 translate-y-0")
 
   return (
-    <section className="min-h-80 bg-redd-500  py-16" ref={refEducacion}>
+    <section className="min-h-80 bg-redd-500  max-md:py-16" ref={refEducacion}>
         <section className="area h-auto bg-bluee-500 flex flex-col justify-center items-center scrollFadeBottom" ref={refScrollEducacion}> 
             <h2 className="text-lg font-semibold tracking-spacing mb-10">EDUCACION</h2>
             <TimeLine/>
